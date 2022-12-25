d9375142bfbde97cf8665851bb326b21e6598fe4
davidaffortunati <affortunati.david@gmail.com>
2022-12-25 20:52:29 +0100

    commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import tensorflow
+
 from Tak import Tak, MoveAction, Direction
 from TakNN import TakNN
 import numpy as np
@@ -6,6 +8,8 @@ from Trainer import Trainer
 
 # Credit to https://github.com/suragnair/alpha-zero-general
 import sys
+
+print(tensorflow.test.gpu_device_name())
 sys.setrecursionlimit(10000)
 nn = TakNN()
 c = Trainer(nn)
